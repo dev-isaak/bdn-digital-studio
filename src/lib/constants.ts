6af06b1f5442b84fa485f309bc4bf794e4ddd3fe
Isaac <isaak_1992@hotmail.es>
2024-12-08 13:20:21 +0100

    bypass api url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const EXAMPLE_PATH = "blog-starter";
 export const CMS_NAME = "Markdown";
-export const API_URL = 'http://localhost:3000'
+// export const API_URL = 'http://localhost:3000'
+export const API_URL = 'https://bdndigitalstudio.com'
