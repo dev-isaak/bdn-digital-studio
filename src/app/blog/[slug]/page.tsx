622cd2609058cb811aac732f1f14da6e8f6ed416
Isaac <isaak_1992@hotmail.es>
2024-12-08 17:15:08 +0100

    deploy api

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,112 +1,113 @@
-import ContactBlock from "@/app/_components/ContactBlock";
-import { API_URL } from "@/lib/constants";
-import { Card, CardHeader, Image } from "@nextui-org/react";
+// import ContactBlock from "@/app/_components/ContactBlock";
+// import { API_URL } from "@/lib/constants";
+// import { Card, CardHeader, Image } from "@nextui-org/react";
 
-export async function generateMetadata({
-	params,
-}: {
-	params: { slug: string };
-}) {
-	const slug = params.slug;
-	const res = await fetch(
-		`${API_URL}/api/get-post?slug=${encodeURIComponent(slug)}`,
-		{
-			method: "GET",
-			headers: {
-				"content-type": "application/json",
-			},
-		}
-	);
-	const { post } = await res.json();
+// export async function generateMetadata({
+// 	params,
+// }: {
+// 	params: { slug: string };
+// }) {
+// 	const slug = params.slug;
+// 	const res = await fetch(
+// 		`${API_URL}/api/get-post?slug=${encodeURIComponent(slug)}`,
+// 		{
+// 			method: "GET",
+// 			headers: {
+// 				"content-type": "application/json",
+// 			},
+// 		}
+// 	);
+// 	const { post } = await res.json();
 
-	return {
-		alternates: {
-			canonical: `https://bdndigitalstudio.com/blog/${slug}`,
-		},
-		title: `${post.title} | BDN Digital Studio`,
-		description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
-		openGraph: {
-			title: post.title,
-			description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
-			images: [
-				{
-					url: post.featuredImage?.node?.mediaItemUrl || "",
-					alt: post.featuredImage?.node?.altText || "Imagen del header",
-				},
-			],
-		},
-	};
-}
+// 	return {
+// 		alternates: {
+// 			canonical: `https://bdndigitalstudio.com/blog/${slug}`,
+// 		},
+// 		title: `${post.title} | BDN Digital Studio`,
+// 		description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
+// 		openGraph: {
+// 			title: post.title,
+// 			description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
+// 			images: [
+// 				{
+// 					url: post.featuredImage?.node?.mediaItemUrl || "",
+// 					alt: post.featuredImage?.node?.altText || "Imagen del header",
+// 				},
+// 			],
+// 		},
+// 	};
+// }
 
-export default async function Page({ params }: any) {
-	const slug = params.slug;
-	const res = await fetch(
-		`${API_URL}/api/get-post?slug=${encodeURIComponent(slug)}`,
-		{
-			method: "GET",
-			headers: {
-				"content-type": "application/json",
-			},
-		}
-	);
-	let {
-		post: {
-			content,
-			title,
-			featuredImage: {
-				node: { altText, mediaItemUrl },
-			},
-		},
-	} = await res.json();
-	// console.log(", respuesta: ", post.content);
+// export default async function Page({ params }: any) {
+// 	const slug = params.slug;
+// 	const res = await fetch(
+// 		`${API_URL}/api/get-post?slug=${encodeURIComponent(slug)}`,
+// 		{
+// 			cache: "force-cache",
+// 			method: "GET",
+// 			headers: {
+// 				"content-type": "application/json",
+// 			},
+// 		}
+// 	);
+// 	let {
+// 		post: {
+// 			content,
+// 			title,
+// 			featuredImage: {
+// 				node: { altText, mediaItemUrl },
+// 			},
+// 		},
+// 	} = await res.json();
+// 	// console.log(", respuesta: ", post.content);
 
-	return (
-		<>
-			<Card radius='none' className='h-96 relative'>
-				{/* Overlay con opacidad */}
-				<div className='absolute inset-0 bg-black opacity-50 z-10'></div>
+// 	return (
+// 		<>
+// 			<Card radius='none' className='h-96 relative'>
+// 				{/* Overlay con opacidad */}
+// 				<div className='absolute inset-0 bg-black opacity-50 z-10'></div>
 
-				{/* Contenido del encabezado */}
-				<CardHeader className='h-96 absolute flex flex-col justify-center align-middle text-center gap-10 z-20'>
-					<div>
-						<h1 className='uppercase xs:text-4xl md:text-4xl text-white font-bold'>
-							{title}
-						</h1>
-					</div>
-				</CardHeader>
+// 				{/* Contenido del encabezado */}
+// 				<CardHeader className='h-96 absolute flex flex-col justify-center align-middle text-center gap-10 z-20'>
+// 					<div>
+// 						<h1 className='uppercase xs:text-4xl md:text-4xl text-white font-bold'>
+// 							{title}
+// 						</h1>
+// 					</div>
+// 				</CardHeader>
 
-				{/* Imagen de fondo */}
-				<Image
-					radius='none'
-					height={500}
-					removeWrapper
-					alt={altText}
-					className='z-0 w-full h-full object-cover'
-					src={mediaItemUrl}
-				/>
-			</Card>
+// 				{/* Imagen de fondo */}
+// 				<Image
+// 					radius='none'
+// 					height={500}
+// 					removeWrapper
+// 					alt={altText}
+// 					className='z-0 w-full h-full object-cover'
+// 					src={mediaItemUrl}
+// 				/>
+// 			</Card>
 
-			<section className=''>
-				<div>
-					<div
-						id='blog-post'
-						dangerouslySetInnerHTML={{ __html: content }}></div>
-				</div>
-			</section>
-			<section className='max-w-[1000px] my-6 md:my-10 mx-1 md:m-auto'>
-				<ContactBlock />
-			</section>
-		</>
-	);
-}
+// 			<section className=''>
+// 				<div>
+// 					<div
+// 						id='blog-post'
+// 						dangerouslySetInnerHTML={{ __html: content }}></div>
+// 				</div>
+// 			</section>
+// 			<section className='max-w-[1000px] my-6 md:my-10 mx-1 md:m-auto'>
+// 				<ContactBlock />
+// 			</section>
+// 		</>
+// 	);
+// }
 
-interface PostProps {
-	title: string;
-	content: string;
-	featuredImage: {
-		node: {
-			mediaItemUrl: string;
-			altText: string;
-		};
-	};
-}
+// interface PostProps {
+// 	title: string;
+// 	content: string;
+// 	featuredImage: {
+// 		node: {
+// 			mediaItemUrl: string;
+// 			altText: string;
+// 		};
+// 	};
+// }
