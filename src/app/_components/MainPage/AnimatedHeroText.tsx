91179ab11a21694ba8c7aa39071803ee24d7cf53
Isaac <isaak_1992@hotmail.es>
2024-12-09 10:51:51 +0100

    fix error types

--- src/app/_components/MainPage/AnimatedHeroText.tsx
+++ src/app/_components/MainPage/AnimatedHeroText.tsx
@@ -3,14 +3,15 @@ import { motion } from "framer-motion";
 
 export default function AnimatedHeroText() {
 	return (
-		<motion.div
-			initial={{ y: 300 }}
-			whileInView={{ y: 0 }}
-			transition={{ duration: 0.5, ease: "easeInOut" }}
-			className='relative flex-grow md:flex-grow-1'>
+		<div className='relative flex-grow md:flex-grow-1'>
+			{/* <motion.div
+				initial={{ y: -5 }}
+				whileInView={{ y: 0 }}
+				transition={{ duration: 0.5, ease: "easeInOut" }}> */}
 			<h4 className='absolute bottom-0 right-0 md:right-[10vw] h-[200px] md:border-l border-t md:border-t-0 border-white-light pr-4 md:px-6 text-white-light text-sm md:text-lg'>
 				Entre las metas de tu empresa <br />y las expectativas de tus clientes.
 			</h4>
-		</motion.div>
+			{/* </motion.div> */}
+		</div>
 	);
 }
