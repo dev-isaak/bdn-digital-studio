0bc42a1e6fa8afaa26fb74b9ca0b645c4163cd72
Isaac <isaak_1992@hotmail.es>
2024-12-10 19:12:43 +0100

    add confetti on form

--- src/app/mensaje-enviado/page.tsx
+++ src/app/mensaje-enviado/page.tsx
@@ -1,18 +1,25 @@
+"use client";
 import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { FaCheckCircle } from "react-icons/fa";
+import Confetti from "react-confetti";
+import useWindowSize from "@/hooks/useWindowSize";
 
 export default function MensajeEnviado() {
-  return (
-    <section className="flex flex-col mt-40 justify-center text-center items-center gap-4">
-      <FaCheckCircle size={100} color="green" />
-      <h1 className="font-bold text-4xl">Mensaje enviado</h1>
-      <p className="text-xl">
-        Hemos recibido tu mensaje. ¡Pronto nos pondremos en contacto contigo!
-      </p>
-      <Button color="primary" variant="bordered" as={Link} href="/">
-        Volver a la página de inicio
-      </Button>
-    </section>
-  );
+	const { width, isMobile } = useWindowSize();
+	const height = isMobile ? 1000 : 1500;
+
+	return (
+		<section className='flex flex-col mt-40 justify-center text-center items-center gap-4'>
+			<Confetti width={width} height={height} />
+			<FaCheckCircle size={100} color='green' />
+			<h1 className='font-bold text-xl'>Mensaje enviado</h1>
+			<p className='text-xl'>
+				Hemos recibido tu mensaje. ¡Pronto nos pondremos en contacto contigo!
+			</p>
+			<Button color='primary' variant='bordered' as={Link} href='/'>
+				Volver a la página de inicio
+			</Button>
+		</section>
+	);
 }
