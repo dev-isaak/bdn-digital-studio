2475ca1baee459b4e53024fbd719d2488b583338
Isaac <isaak_1992@hotmail.es>
2024-12-10 09:20:15 +0100

    fix types

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,85 +1,104 @@
-import { Card, CardHeader, Image } from "@nextui-org/react";
+import { Button, Card, CardHeader, Image, Link } from "@nextui-org/react";
 import ContactBlock from "../../_components/ContactBlock";
 import { WordPressPostProps } from "../../../interfaces/wp_post";
 import { getPost } from "../data";
 import { Metadata } from "next";
 
 interface ParamsProps {
-  params: {
-    id: string;
-  };
+	params: {
+		id: string;
+	};
 }
 
 export async function generateMetadata(
-  { params }: ParamsProps, // Tipamos correctamente los parámetros
-  parent: Promise<Metadata> // `parent` es de tipo `Promise<Metadata>`
+	{ params }: ParamsProps,
+	parent: Promise<Metadata>
 ): Promise<Metadata> {
-  // La función retorna una Promise de tipo Metadata
-  const post: WordPressPostProps = await getPost(params.id);
+	const post = await getPost(params.id);
+	if (!post)
+		return {
+			title: `Página no encontrada | BDN Digital Studio`,
+		};
 
-  return {
-    alternates: {
-      canonical: `https://bdndigitalstudio.com/blog/${params.id}`,
-    },
-    title: `${post.title} | BDN Digital Studio`,
-    description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
-    openGraph: {
-      title: post.title,
-      description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
-      images: [
-        {
-          url: post.featuredImage?.node?.mediaItemUrl || "",
-          alt: post.featuredImage?.node?.altText || "Imagen del header",
-        },
-      ],
-    },
-  };
+	return {
+		alternates: {
+			canonical: `https://bdndigitalstudio.com/blog/${params.id}`,
+		},
+		title: `${post.title} | BDN Digital Studio`,
+		description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
+		openGraph: {
+			title: post.title,
+			description: post.seo?.metaDesc || "Blog | BDN Digital Studio",
+			images: [
+				{
+					url: post.featuredImage?.node?.mediaItemUrl || "",
+					alt: post.featuredImage?.node?.altText || "Imagen del header",
+				},
+			],
+		},
+	};
 }
 
 export default async function Page({ params }: ParamsProps) {
-  const slug = params.id;
-  const {
-    title,
-    content,
-    featuredImage: {
-      node: { altText, mediaItemUrl },
-    },
-  }: WordPressPostProps = await getPost(slug);
+	const slug = params.id;
+	const post = await getPost(slug);
+	if (!post) {
+		return <RenderPaginaNoEncontrada />;
+	}
+	const {
+		title,
+		content,
+		featuredImage: {
+			node: { altText, mediaItemUrl },
+		},
+	} = post;
 
-  return (
-    <>
-      <Card radius="none" className="h-96 relative">
-        <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
+	return (
+		<>
+			<Card radius='none' className='h-96 relative'>
+				<div className='absolute inset-0 bg-black opacity-50 z-10'></div>
 
-        <CardHeader className="h-96 absolute flex flex-col justify-center align-middle text-center gap-10 z-20">
-          <div>
-            <h1 className="uppercase xs:text-4xl md:text-4xl text-white font-bold">
-              {title}
-            </h1>
-          </div>
-        </CardHeader>
+				<CardHeader className='h-96 absolute flex flex-col justify-center align-middle text-center gap-10 z-20'>
+					<div>
+						<h1 className='uppercase xs:text-4xl md:text-4xl text-white font-bold'>
+							{title}
+						</h1>
+					</div>
+				</CardHeader>
 
-        <Image
-          radius="none"
-          height={500}
-          removeWrapper
-          alt={altText}
-          className="z-0 w-full h-full object-cover"
-          src={mediaItemUrl}
-        />
-      </Card>
+				<Image
+					radius='none'
+					height={500}
+					removeWrapper
+					alt={altText}
+					className='z-0 w-full h-full object-cover'
+					src={mediaItemUrl}
+				/>
+			</Card>
 
-      <section className="">
-        <div>
-          <div
-            id="blog-post"
-            dangerouslySetInnerHTML={{ __html: content }}
-          ></div>
-        </div>
-      </section>
-      <section className="max-w-[1000px] my-6 md:my-10 mx-1 md:m-auto">
-        <ContactBlock />
-      </section>
-    </>
-  );
+			<section>
+				<div>
+					<div
+						id='blog-post'
+						dangerouslySetInnerHTML={{ __html: content }}></div>
+				</div>
+			</section>
+			<section className='max-w-[1000px] my-6 md:my-10 mx-1 md:m-auto'>
+				<ContactBlock />
+			</section>
+		</>
+	);
 }
+
+const RenderPaginaNoEncontrada = () => {
+	return (
+		<section className='mt-28 text-center font-semibold'>
+			<h1 className='text-4xl mb-6'>
+				No se ha encontrado el post que buscabas.
+			</h1>
+			<Button color='primary' as={Link} href='/blog'>
+				Ir al blog
+			</Button>
+		</section>
+	);
+};
