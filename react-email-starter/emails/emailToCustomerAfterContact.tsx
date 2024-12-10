420678a9768920260cd4ffdba41fec3a02eab666
Isaac <isaak_1992@hotmail.es>
2024-12-10 22:42:02 +0100

    config email

---
+++ react-email-starter/emails/emailToCustomerAfterContact.tsx
@@ -0,0 +1,84 @@
+import {
+	Body,
+	Container,
+	Head,
+	Heading,
+	Html,
+	Img,
+	Preview,
+	Section,
+	Text,
+} from "@react-email/components";
+import * as React from "react";
+import FooterEmail from "./components/footer";
+
+interface NotionMagicLinkEmailProps {
+	userName?: string;
+}
+
+export const EmailToCustomerAfterContact = ({
+	userName,
+}: NotionMagicLinkEmailProps) => (
+	<Html>
+		<Head />
+		<Preview>BDN Digital Studio | ¡Gracias por contactarnos!</Preview>
+		<Body style={main}>
+			<Container style={container}>
+				{/* <Img
+					alt='Imagen de cabecera'
+					width='100%'
+					height='300'
+					src='/static/building-email.png'
+					style={image}
+				/> */}
+				<Heading style={h1}>
+					¡Hola {userName}, gracias por contactarnos!
+				</Heading>
+				<Text style={text}>
+					Gracias por ponerte en contacto con nosotros. Hemos recibido tu
+					solicitud y estamos emocionados por ayudarte a dar vida a tu proyecto.
+					Un miembro de nuestro equipo revisará tu mensaje y se pondrá en
+					contacto contigo en las próximas 24-48 horas para entender mejor tus
+					necesidades y discutir los siguientes pasos. Mientras tanto, si tienes
+					alguna información adicional que te gustaría compartir, no dudes en
+					responder a este correo. ¡Estamos aquí para hacer realidad tus ideas!
+				</Text>
+				<FooterEmail />
+			</Container>
+		</Body>
+	</Html>
+);
+
+export default EmailToCustomerAfterContact;
+
+const image = {
+	borderRadius: "10px",
+};
+
+const main = {
+	backgroundColor: "#ffffff",
+};
+
+const container = {
+	paddingLeft: "12px",
+	paddingRight: "12px",
+	margin: "0 auto",
+};
+
+const h1 = {
+	color: "#333",
+	fontFamily:
+		"-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+	fontSize: "24px",
+	fontWeight: "bold",
+	margin: "40px 0",
+	padding: "0",
+};
+
+const text = {
+	color: "#333",
+	fontFamily:
+		"-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
+	fontSize: "14px",
+	margin: "24px 0",
+};
--- src/app/emails/emailToCustomerAfterContact.ts
+++ src/app/emails/emailToCustomerAfterContact.ts
@@ -1,3 +1,5 @@
+import EmailToCustomerAfterContact from "../../../react-email-starter/emails/emailToCustomerAfterContact";
+
 interface EmailProps {
   resend: any;
   email: string;
@@ -14,19 +16,7 @@ export const emailToCustomerAfterContact = async ({
       from: "hola@bdndigitalstudio.com",
       to: [email],
       subject: "BDN Digital Studio | ¡Gracias por contactarnos!",
-      html: `
-    <h1>¡Hola ${name}!</h1>
-    <p>Gracias por ponerte en contacto con <strong>BDN Digital Studio</strong>. Hemos recibido tu solicitud y estamos emocionados por ayudarte a dar vida a tu proyecto.</p>
-    <p>Un miembro de nuestro equipo revisará tu mensaje y se pondrá en contacto contigo en las próximas 24-48 horas para entender mejor tus necesidades y discutir los siguientes pasos.</p>
-    <p>Mientras tanto, si tienes alguna información adicional que te gustaría compartir, no dudes en responder a este correo.</p>
-    <br>
-    <p>¡Estamos aquí para hacer realidad tus ideas!</p>
-    <br>
-    <br>
-    <p>Saludos cordiales,</p>
-    <p>El equipo de <strong>BDN Digital Studio</strong></p>
-    <p><a href="https://bdndigitalstudio.com">Visítanos en nuestro sitio web</a></p>
-  `,
+      react: EmailToCustomerAfterContact({ userName: name })
     });
     if (res.error === null) return true;
   } catch (error) {
