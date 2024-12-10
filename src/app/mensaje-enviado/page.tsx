f92548438e746ac6316caba6b4cb836950ac585e
Isaac <isaak_1992@hotmail.es>
2024-12-10 19:28:08 +0100

    replace route

--- src/app/mensaje-enviado/page.tsx
+++ src/app/mensaje-enviado/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { Button } from "@nextui-org/react";
-import Link from "next/link";
 import { FaCheckCircle } from "react-icons/fa";
 import Confetti from "react-confetti";
 import useWindowSize from "@/hooks/useWindowSize";
+import { useRouter } from "next/navigation";
 
 export default function MensajeEnviado() {
+	const router = useRouter();
 	const { width, isMobile } = useWindowSize();
 	const height = isMobile ? 1000 : 1500;
+
+	const handleRedirect = () => {
+		router.replace("/");
+	};
 
 	return (
 		<section className='flex flex-col mt-40 justify-center text-center items-center gap-4'>
@@ -17,7 +22,7 @@ export default function MensajeEnviado() {
 			<p className='text-xl'>
 				Hemos recibido tu mensaje. ¡Pronto nos pondremos en contacto contigo!
 			</p>
-			<Button color='primary' variant='bordered' as={Link} href='/'>
+			<Button color='primary' variant='bordered' onPress={handleRedirect}>
 				Volver a la página de inicio
 			</Button>
 		</section>
