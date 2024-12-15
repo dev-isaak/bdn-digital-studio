d01812225395ed6afa5edafb2ce84b6d07a1c6fd
Isaac <isaak_1992@hotmail.es>
2024-12-15 23:38:09 +0100

    add section

--- src/app/_components/CardWithTopNumber/index.tsx
+++ src/app/_components/CardWithTopNumber/index.tsx
@@ -1,25 +1,25 @@
 import { Card, CardHeader, CardBody } from "@nextui-org/react";
 
 interface CardProps {
-  number: string;
-  title: string;
-  text: string;
+	number: string;
+	title: string;
+	text: string;
 }
 
 export default function CardWithTopNumber({ number, title, text }: CardProps) {
-  return (
-    <Card shadow="none" className="max-w-[400px]">
-      <CardHeader className="flex gap-3">
-        <div className="flex flex-col">
-          <p className="text-xl">{number}</p>
-          <h4 className="uppercase text-2xl text-default-900 font-semibold">
-            {title}
-          </h4>
-        </div>
-      </CardHeader>
-      <CardBody>
-        <div dangerouslySetInnerHTML={{ __html: text }}></div>
-      </CardBody>
-    </Card>
-  );
+	return (
+		<Card shadow='none' className='max-w-[400px] h-[220px]'>
+			<CardHeader className='flex gap-3'>
+				<div className='flex flex-col'>
+					<p className='text-xl'>{number}</p>
+					<h4 className='uppercase text-2xl text-default-900 font-semibold'>
+						{title}
+					</h4>
+				</div>
+			</CardHeader>
+			<CardBody>
+				<div dangerouslySetInnerHTML={{ __html: text }}></div>
+			</CardBody>
+		</Card>
+	);
 }
--- src/app/_components/MainPage/index.tsx
+++ src/app/_components/MainPage/index.tsx
@@ -3,35 +3,39 @@ import ContactBlock from "../ContactBlock";
 import AnimatedHeroText from "./AnimatedHeroText";
 import AnimatedTitle from "./AnimatedTitle";
 import Faqs from "./Faqs";
+import Purpose from "./Purpose";
 import Services from "./Services";
 import FirstSection from "./Value";
 
 export default function MainPage() {
-  return (
-    <div>
-      <header>
-        <div className="flex flex-col md:flex-row w-full h-screen bg-primary top-0">
-          {/* Left Section */}
-          <AnimatedTitle />
-          {/* Right Section */}
-          <AnimatedHeroText />
-        </div>
-      </header>
-      <div className="m-auto max-w-7xl px-4 flex flex-col gap-10 my-10">
-        <section>
-          <FirstSection />
-        </section>
-        <section className="overflow-hidden ">
-          <CarousselTitle title="servicios" />
-          <Services />
-        </section>
-        <section>
-          <ContactBlock />
-        </section>
-        <section>
-          <Faqs />
-        </section>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<div className='flex flex-col md:flex-row w-full h-screen bg-primary top-0'>
+					{/* Left Section */}
+					<AnimatedTitle />
+					{/* Right Section */}
+					<AnimatedHeroText />
+				</div>
+			</header>
+			<div className='m-auto max-w-7xl px-4 flex flex-col gap-10 my-10'>
+				<section>
+					<FirstSection />
+				</section>
+				<section>
+					<Purpose />
+				</section>
+				<section className='overflow-hidden '>
+					<CarousselTitle title='servicios' />
+					<Services />
+				</section>
+				<section>
+					<ContactBlock />
+				</section>
+				<section>
+					<Faqs />
+				</section>
+			</div>
+		</div>
+	);
 }
